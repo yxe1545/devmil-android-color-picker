094eeb9661981851fd1d6861ba5fc3af9f86efea
develmil <develmil@googlemail.com>
2011-01-05 19:50:17 +0100

    added headers

--- src/de/devmil/common/ui/color/ColorSelectorDialog.java
+++ src/de/devmil/common/ui/color/ColorSelectorDialog.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Devmil (Michael Lamers) 
+ * Mail: develmil@googlemail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.devmil.common.ui.color;
 
 import android.app.Dialog;
@@ -86,7 +102,7 @@ public class ColorSelectorDialog extends Dialog {
 	private void colorChangedInternal(int color)
 	{
 		btnNew.setBackgroundColor(color);
-		btnNew.setTextColor(~color | 0xFF000000);
+		btnNew.setTextColor(~color | 0xFF000000); //without alpha
 		this.color = color;
 	}
 	
